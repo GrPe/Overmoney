7be8671545c8e4c9bc5858bf93edc4be3df15ca7
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-03-08 14:35:13 +0100

    Added currencies

---
+++ src/Overmoney.Api/DataAccess/Currencies/CurrencyRepository.cs
@@ -0,0 +1,36 @@
+﻿using Overmoney.Api.DataAccess.Currencies.Models;
+
+namespace Overmoney.Api.DataAccess.Currencies;
+
+public interface ICurrencyRepository : IRepository
+{
+    Task<Currency> GetAsync(int id, CancellationToken cancellationToken);
+    Task<IEnumerable<Currency>> GetAllAsync(CancellationToken cancellationToken);
+    Task<Currency> CreateAsync(Currency currency, CancellationToken cancellationToken);
+    Task<Currency> UpdateAsync(Currency currency, CancellationToken cancellationToken);
+}
+
+public sealed class CurrencyRepository : ICurrencyRepository
+{
+    private static readonly List<Currency> _connection = [];
+
+    public Task<Currency> CreateAsync(Currency currency, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Currency>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Currency> GetAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Currency> UpdateAsync(Currency currency, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Overmoney.Api/DataAccess/Currencies/Models/Currency.cs
@@ -0,0 +1,9 @@
+﻿namespace Overmoney.Api.DataAccess.Currencies.Models;
+
+public sealed class Currency
+{
+    public int Id { get; init; }
+    public string Code { get; init; } = null!;
+    public string Name { get; init; } = null!;
+    public double ExchangeRate { get; init; }
+}
