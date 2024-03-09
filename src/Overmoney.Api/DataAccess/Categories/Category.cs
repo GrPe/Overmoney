ad7d95774e4c3c10719b8689b93e4579768413eb
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-03-09 19:20:05 +0100

    Added transaction, payee and category entities

---
+++ src/Overmoney.Api/DataAccess/Categories/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace Overmoney.Api.DataAccess.Categories;
+
+public class Category
+{
+    public int Id { get; init; }
+    public int UserId { get; init; }
+    public string Name { get; init; } = null!;
+
+    public Category(int id, int userId, string name)
+    {
+        Id = id;
+        UserId = userId;
+        Name = name;
+    }
+}
