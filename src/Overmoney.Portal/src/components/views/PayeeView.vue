e4a6ea3227bc0826560e07df8b58a71ca03e304a
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-05-07 21:10:01 +0200

    update

--- src/Overmoney.Portal/src/components/views/PayeeView.vue
+++ src/Overmoney.Portal/src/components/views/PayeeView.vue
@@ -1,7 +1,7 @@
 <template>
-    <PayeesList :payees="payess" ></PayeesList>
+    <PayeesList :payees="payess"></PayeesList>
 </template>
-  
+
 <script lang="ts">
 import PayeesList from '../lists/PayeesList.vue'
 import type { Payee } from '../../data_access/models/payee'
@@ -26,19 +26,19 @@ export default {
 
 <style scoped>
 body #app header {
-margin: 0;
-padding: 0;
+    margin: 0;
+    padding: 0;
 }
 
 header {
-height: 100vh;
-width: 100vw;
-display: flex;
-align-items: center;
-justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .wrapper {
-padding-left: 30px;
+    padding-left: 30px;
 }
 </style>
