386d75e1edc5b566734a8865459ca01d476f981c
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-05-10 15:22:55 +0200

    Added buttons on transaction list

--- src/Overmoney.Portal/src/components/lists/TransactionList.vue
+++ src/Overmoney.Portal/src/components/lists/TransactionList.vue
@@ -10,6 +10,7 @@
             <th>Amount</th>
             <th>Note</th>
             <th>Attachments?</th>
+            <th>Actions</th>
         </tr>
         <tr v-for="transaction in transactions" :key="transaction.id">
             <td>{{ transaction.id }}</td>
@@ -20,7 +21,15 @@
             <td>{{ transaction.transactionType }}</td>
             <td>{{ transaction.amount }}</td>
             <td>{{ transaction.note }}</td>
-            <td>{{ transaction.attachments?.length == 0 ? "Yes" : "No" }}</td>
+            <td>{{ transaction.attachments?.length == 0 ? "Yes" : "No" }}</td>            
+            <td>
+                <button @click="updateTransaction(transaction.id)">
+                    Edit
+                </button>
+                <button @click="removeTransaction(transaction.id)">
+                    Delete
+                </button>
+            </td>
         </tr>
     </table>
 </template>
@@ -31,6 +40,14 @@ import type {Transaction} from '../../data_access/models/transaction'
 export default {
     props: {
         transactions: Array<Transaction>
+    },
+    methods: {
+        async updateTransaction(id: number) {
+            this.$emit('updateTransaction', id);
+        },
+        async removeTransaction(id: number) {
+            this.$emit('removeTransaction', id);
+        }
     }
 }
 </script>
