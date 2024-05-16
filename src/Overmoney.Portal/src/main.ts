5da808fe5135b7db58266c145bc69b2e04fab483
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-05-16 19:53:27 +0200

    Added Pinia

--- src/Overmoney.Portal/src/main.ts
+++ src/Overmoney.Portal/src/main.ts
@@ -1,22 +1,14 @@
 //import './assets/main.css'
-import 'primevue/resources/themes/aura-light-green/theme.css'
-import 'primevue/resources/primevue.min.css';
-//import 'primeicons/primeicons.css';     
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
-
-import PrimeVue from 'primevue/config'
-import PrimeMenu from "primevue/menubar";
-import Panel from "primevue/panel"
-
 import router from './router';
 
-const app = createApp(App);
-app.use(PrimeVue)
-app.use(router);
+const pinia = createPinia();
 
-app.component("PrimeMenu", PrimeMenu);
-app.component("PrimePanel", Panel);
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
 
 app.mount('#app')
