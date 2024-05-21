4e7cac32427c9b0963528cc2db46d3f4a73e27ea
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-05-21 21:08:34 +0200

    Another fix

--- src/Overmoney.IntegrationTests/Configurations/ApiWebApplicationFactory.cs
+++ src/Overmoney.IntegrationTests/Configurations/ApiWebApplicationFactory.cs
@@ -25,22 +25,16 @@ internal class ApiWebApplicationFactory : WebApplicationFactory<Program>
                 services.Remove(dbContext);
             }
 
-            var dbContextOptions = services.FirstOrDefault(x => x.ServiceType.Name.Contains("DbContextOptions"));
-            if (dbContextOptions is not null)
-            {
-                services.Remove(dbContextOptions);
-            }
-
             var identityDb = services.FirstOrDefault(x => x.ServiceType.Name.Contains("ApplicationIdentityDbContext"));
             if (identityDb is not null)
             {
                 services.Remove(identityDb);
             }
 
-            dbContextOptions = services.FirstOrDefault(x => x.ServiceType.Name.Contains("DbContextOptions"));
-            if (dbContextOptions is not null)
+            var options = services.Where(x => x.ServiceType.Name.Contains("DbContextOptions")).ToList();
+            foreach (var option in options)
             {
-                services.Remove(dbContextOptions);
+                services.Remove(option);
             }
 
             services.AddDataAccess($"Host={_postgresHostname}:{_postgresPort};Database=overmoney;Username=dev;Password=dev;Timeout=300;CommandTimeout=300", applyMigrations: true);
